bcfdf4712619ddf25734319f6bee02fd74c66645
IATec - Wictor Mendes Vieira <wictor.vieira@iatec.com>
2023-05-30 13:48:12 -0300

    Setup

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import Habit from "./components/Habit"
+import './styles/global.css'
+
+function App() {
+  return (
+    <>
+    <Habit completed={3}></Habit>
+    </>
+  )
+}
+
+export default App
